1cba9fa1434c6dc916c20b0b1dc6a94b7cd90c34
rubi Prajapati <rubipra812@gmail.com>
2026-07-15 17:55:58 +0530

    modifie index file

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.get("/todos", (req, res) => {
   });
 });
 app.get("/todos/:id", (req, res) => {
-  const {id} = req.params;
-  let num=id.slice(1)
-  let num2=Number(num);
-  const todo_task = todo.find((ele) => ele.id ===num2);
+  const { id } = req.params;
+  let num = id.slice(1);
+  let num2 = Number(num);
+  const todo_task = todo.find((ele) => ele.id === num2);
   console.log(todo_task);
   if (!todo_task) {
     return res.status(404).json({
@@ -32,16 +32,68 @@ app.get("/todos/:id", (req, res) => {
 app.post("/todos", (req, res) => {
   let newItem = {
     userId: 1,
-    id: 9,
-    title: "illo expedita consequatur quia in",
-    completed: false,
+    id: todo.length + 1,
+    ...req.body,
+    title: todo,
+    ...req.body,
+    completed: todo,
+    ...req.body,
   };
-  todo = [...todo, newItem];
-  //   todo.push(newItem)
+
+  todo.push(newItem);
 
   res.status(201).json({
     success: true,
     data: todo,
+  });
+});
+
+app.put("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  let num = id.slice(1);
+  let num2 = Number(num);
+  console.log(data);
+  const userTodo = todo.find((ele) => ele.id === num2);
+  if (!userTodo) {
+    res.status(404).json({
+      success: true,
+      message: "user not found",
+    });
+  }
+
+  const updateTodo = todo.map((ele) => {
+    if (ele.id === num2) {
+      return { ...ele, ...data };
+    }
+
+    return ele;
+  });
+  res.status(200).json({
+    success: true,
+    data: updateTodo,
+    // message:"updated user successfully"
+  });
+});
+app.delete("/todos/:id", (req, res) => {
+  const {id} =req.params;
+ let num = id.slice(1);
+  let num2 = Number(num);
+  console.log(id);
+  const userTodo = todo.find((ele) => ele.id === num2);
+  if (!userTodo) {
+    return res.status(404).json({
+      success:false,
+      message: "user not found",
+    });
+  }
+
+   todo = todo.filter((ele) => ele.id !== num2);
+
+  res.status(200).json({
+    success: true,
+    data:todo,
+    // message:"updated user successfully"
   });
 });
 
